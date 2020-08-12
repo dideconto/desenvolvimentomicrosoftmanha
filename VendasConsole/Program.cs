60feb8411ae6525cf8d2d27232f87c9137914304
Diogo Deconto <dideconto@gmail.com>
2020-08-12 10:20:10 -0300

    Adição do menu

--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -6,7 +6,35 @@ namespace VendasConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" --- PROJETO DE VENDAS --- \n");
+                Console.WriteLine("1 - Cadastrar cliente");
+                Console.WriteLine("2 - Listar clientes");
+                Console.WriteLine("0 - Sair\n");
+                Console.WriteLine("Escolha uma opção:");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine(" --- CADASTRAR CLIENTE --- \n");
+                        break;
+                    case 2:
+                        Console.WriteLine(" --- LISTAR CLIENTES --- \n");
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo...\n");
+                        break;
+                    default:
+                        Console.WriteLine(" --- OPÇÃO INVÁLIDA!!! --- \n");
+                        break;
+                }
+                Console.WriteLine("\nAperte uma tecla para continuar...");
+                Console.ReadKey();
+            } while (opcao != 0);
         }
     }
 }
