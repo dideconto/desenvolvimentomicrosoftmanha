15c70de2d71853a50f481544fe31713b0266091e
Diogo Deconto <dideconto@gmail.com>
2020-09-21 16:12:34 -0300

    Projeto de vendas WPF

---
+++ VendasWPF/Views/frmCadastrarCliente.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+
+namespace VendasWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for frmCadastrarCliente.xaml
+    /// </summary>
+    public partial class frmCadastrarCliente : Window
+    {
+        public frmCadastrarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n1 = Convert.ToInt32(txtNumero1.Text);
+                int n2 = Convert.ToInt32(txtNumero2.Text);
+                int soma = n1 + n2;
+                MessageBox.Show($"Resultado: {soma}");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Somente números!!!");
+            }
+        }
+    }
+}
---
+++ VendasWPF/Views/frmPrincipal.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace VendasWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for frmPrincipal.xaml
+    /// </summary>
+    public partial class frmPrincipal : Window
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void menuSair_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Vendas WPF",
+                   MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void menuCadastrarCliente_Click(object sender, RoutedEventArgs e)
+        {
+            frmCadastrarCliente frm = new frmCadastrarCliente();
+            frm.ShowDialog();
+        }
+    }
+}
