c16e69d1625e618530f88a8def83126e7ecf461f
Diogo Deconto <dideconto@gmail.com>
2020-10-15 17:28:15 -0300

    Projeto Vendas Web - ASP.NET Core MVC

---
+++ VendasWeb/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWeb.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
