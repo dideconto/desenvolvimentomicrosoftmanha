fe301b38e5a1ae7375642b5b33541a534268c771
Diogo Deconto <dideconto@gmail.com>
2020-10-21 17:24:02 -0300

    Cadastro e listagem de produtos

--- VendasWeb/Controllers/ProdutoController.cs
+++ VendasWeb/Controllers/ProdutoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using VendasWeb.Models;
 
 namespace VendasWeb.Controllers
@@ -6,16 +8,33 @@ namespace VendasWeb.Controllers
     public class ProdutoController : Controller
     {
         private readonly Context _context;
-        public ProdutoController(Context context)
+        public ProdutoController(Context context) => _context = context;
+        public IActionResult Index()
         {
-            _context = context;
+            List<Produto> produtos = _context.Produtos.ToList();
+            ViewBag.Produtos = produtos;
+            ViewBag.Quantidade = produtos.Count;
+            return View();
         }
-        public IActionResult Index() => View();
         public IActionResult Cadastrar() => View();
         [HttpPost]
-        public IActionResult Cadastrar(string txtNome, int txtQuantidade,
-            double txtPreco, string txtDescricao)
+        public IActionResult Cadastrar(string txtNome, int txtQuantidade, double txtPreco, string txtDescricao)
         {
+            Produto produto = new Produto
+            {
+                Nome = txtNome,
+                Descricao = txtDescricao,
+                Preco = txtPreco,
+                Quantidade = txtQuantidade
+            };
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Produto");
+        }
+
+        public IActionResult Remover(int id)
+        {
+            //Completar com o código da remoção
             return RedirectToAction("Index", "Produto");
         }
     }
